3c5a82036127b0fc8e9fe183536a3e718d5f98d3
Ibrahimfaqath <ibrahimfaqath@gmail.com>
2026-04-09 13:35:31 +0700

    start

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+function App() {
+  const [text, setText] = useState("Ibrahim");
+
+  function gantiNama() {
+   if (text === "Ibrahim") {
+    setText("Dunia");
+  } else {
+    setText("Ibrahim");
+  }
+  }
+
+  return (
+    <div>
+      <h1>Hello {text}</h1>
+      <button onClick={gantiNama}>
+        Klik aku
+      </button>
+    </div>
+  );
+}
+
+export default App;
